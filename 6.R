33bf53a7bcd9f153dfc3c525bb3887e6b1767640
Daniel Bømark <daniel@Daniels-MacBook-Pro.local>
2023-10-04 08:10:19 +0200

    New feature

--- 6.R
+++ 6.R
@@ -4,3 +4,11 @@ x <- rnorm(100)
 y <- rnorm(100)
 
 # Important feature
+
+
+# Competition
+library(tidyverse)
+a <- tibble(name =c("a", "b", "c"), id = c("d","e","f"))
+# USe sample afterwards sample(a$name, size = 1)
+
+# New feature
