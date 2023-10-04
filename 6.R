550a4819120bbebd890790a67b0f777961e14d33
Daniel Bømark <daniel@Daniels-MacBook-Pro.local>
2023-10-04 12:23:40 +0200

    Another improvement

--- 6.R
+++ 6.R
@@ -6,14 +6,7 @@ y <- rnorm(500)
 
 # Important feature
 
-<<<<<<< HEAD
-
 # Competition
 library(tidyverse)
 a <- tibble(name =c("a", "b", "c"), id = c("d","e","f"))
 # USe sample afterwards sample(a$name, size = 1)
-
-# Normal
-=======
-# An alternative way
->>>>>>> alternative-develop
